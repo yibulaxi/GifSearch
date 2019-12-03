30f952a89a18848f71b81b8997eab3e049915ef6
devallever <devallever@163.com>
2019-12-03 10:23:39 +0800

    内容：修改广告配置

--- app/src/main/java/com/allever/app/template/ad/AdConstants.kt
+++ app/src/main/java/com/allever/app/template/ad/AdConstants.kt
@@ -5,11 +5,14 @@ import com.allever.lib.ad.AdBusiness
 
 object AdConstants {
 
+    private val APPID = ""
+    private const val COMMON_BANNER = "ca-app-pub-5576030072710638/7114444166"
+    private const val COMMON_INSERT = "ca-app-pub-5576030072710638/8257819654"
+    private const val COMMON_VIDEO = "ca-app-pub-5576030072710638/8639278822"
+
     val AD_NAME_INSERT = "AD_NAME_INSERT"
 
-
     val AD_NAME_BANNER = "AD_NAME_BANNER"
-
 
     val AD_NAME_VIDEO = "AD_NAME_VIDEO"
 
@@ -29,7 +32,7 @@ object AdConstants {
             "      \"chain\": [\n" +
             "        {\n" +
             "          \"business\": \"${AdBusiness.A}\",\n" +
-            "          \"adPosition\": \"ca-app-pub-8815582923430605/1544706681\"\n" +
+            "          \"adPosition\": \"$COMMON_INSERT\"\n" +
             "        }\n" +
             "      ]\n" +
             "    },\n" +
@@ -39,7 +42,7 @@ object AdConstants {
             "      \"chain\": [\n" +
             "        {\n" +
             "          \"business\": \"${AdBusiness.A}\",\n" +
-            "          \"adPosition\": \"ca-app-pub-8815582923430605/1795988120\"\n" +
+            "          \"adPosition\": \"$COMMON_BANNER\"\n" +
             "        }\n" +
             "      ]\n" +
             "    },\n" +
@@ -49,7 +52,7 @@ object AdConstants {
             "      \"chain\": [\n" +
             "        {\n" +
             "          \"business\": \"${AdBusiness.A}\",\n" +
-            "          \"adPosition\": \"ca-app-pub-8815582923430605/9291334763\"\n" +
+            "          \"adPosition\": \"$COMMON_VIDEO\"\n" +
             "        }\n" +
             "      ]\n" +
             "    }\n" +
