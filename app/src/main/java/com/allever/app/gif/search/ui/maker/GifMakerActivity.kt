ed3520e549f5d58334d833c85e2d877fbd6a143f
devallever <devallever@163.com>
2021-08-21 21:59:54 +0800

    video转gif-提交遗漏代码

---
+++ app/src/main/java/com/allever/app/gif/search/ui/maker/GifMakerActivity.kt
@@ -0,0 +1,84 @@
+package com.allever.app.gif.search.ui.maker
+
+import android.content.Context
+import android.os.Bundle
+import com.allever.app.gif.search.BR
+import com.allever.app.gif.search.R
+import com.allever.app.gif.search.app.BaseDataActivity2
+import com.allever.app.gif.search.databinding.ActivityGifMakerBinding
+import com.allever.app.gif.search.function.media.MediaBean
+import com.allever.app.gif.search.ui.maker.model.GifMakerViewModel
+import com.jaygoo.widget.OnRangeChangedListener
+import com.jaygoo.widget.RangeSeekBar
+import com.xm.lib.base.config.DataBindingConfig
+import com.xm.lib.manager.IntentManager
+
+class GifMakerActivity : BaseDataActivity2<ActivityGifMakerBinding, GifMakerViewModel>() {
+
+    lateinit var mVideoViewHolder: VideoViewHolder
+
+    companion object {
+        private const val EXTRA_DATA = "EXTRA_DATA"
+        fun start(context: Context, data: MediaBean) {
+            val bundle = Bundle()
+            bundle.putParcelable(EXTRA_DATA, data)
+            IntentManager.startActivity(context, GifMakerActivity::class.java, bundle)
+        }
+    }
+
+    override fun statusColor() = R.color.trans
+
+    override fun initDataAndEvent() {
+        mViewModel.mediaBean = intent?.getParcelableExtra(EXTRA_DATA)
+        mVideoViewHolder = VideoViewHolder()
+        mVideoViewHolder.initVideo(
+            mBinding.videoView, mViewModel.mediaBean?.path, mBinding.ivPlayPause
+        )
+        mBinding.ivPlayPause
+        mBinding.rangeSeekBar.setRange(0F, mViewModel.mediaBean?.duration?.toFloat() ?: 0F, 1F)
+        mBinding.rangeSeekBar.setProgress(0F, mViewModel.mediaBean?.duration?.toFloat() ?: 0F)
+        mViewModel.startPosition = 0
+        mViewModel.endPosition = mViewModel.mediaBean?.duration?.toInt()?:1
+        mViewModel.startText.value = "0"
+        mViewModel.endText.value = (mViewModel.endPosition / 1000f).toString()
+        mViewModel.durationText.value = "${(mViewModel.endPosition - mViewModel.startPosition) / 1000f}秒"
+
+        mBinding.rangeSeekBar.setOnRangeChangedListener(object : OnRangeChangedListener {
+            override fun onRangeChanged(
+                view: RangeSeekBar?,
+                leftValue: Float,
+                rightValue: Float,
+                isFromUser: Boolean
+            ) {
+                mViewModel.startPosition = leftValue.toInt()
+                mViewModel.endPosition = rightValue.toInt()
+                mVideoViewHolder.seekTo(leftValue.toInt())
+                mViewModel.startText.value = (leftValue / 1000).toString()
+                mViewModel.endText.value = (rightValue / 1000).toString()
+                mViewModel.durationText.value = "${(mViewModel.endPosition - mViewModel.startPosition) / 1000f}秒"
+            }
+
+            override fun onStartTrackingTouch(view: RangeSeekBar?, isLeft: Boolean) {
+                mVideoViewHolder.pause()
+            }
+
+            override fun onStopTrackingTouch(view: RangeSeekBar?, isLeft: Boolean) {
+                mVideoViewHolder.play()
+            }
+
+        })
+    }
+
+    fun pause() {
+        mVideoViewHolder.pause()
+    }
+
+
+    override fun destroyView() {
+        mVideoViewHolder.stop()
+        mVideoViewHolder.destroy()
+    }
+
+    override fun initDataBindingConfig() =
+        DataBindingConfig(R.layout.activity_gif_maker, BR.gifMakerViewModel)
+}
---
+++ app/src/main/java/com/allever/app/gif/search/ui/maker/model/GifMakerViewModel.kt
@@ -0,0 +1,68 @@
+package com.allever.app.gif.search.ui.maker.model
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.allever.app.gif.search.R
+import com.allever.app.gif.search.event.GifMakeEvent
+import com.allever.app.gif.search.function.maker.GifMakeHelper
+import com.allever.app.gif.search.function.media.MediaBean
+import com.allever.lib.common.util.FileUtils
+import com.allever.lib.common.util.getString
+import com.allever.lib.common.util.log
+import com.xm.lib.base.inters.IBaseView
+import com.xm.lib.base.model.BaseViewModelKt
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.greenrobot.eventbus.EventBus
+import java.io.File
+
+class GifMakerViewModel: BaseViewModelKt<IBaseView>() {
+    var mediaBean: MediaBean? = null
+    var startPosition = 0
+    var endPosition = 0
+    val startText = MutableLiveData<String>()
+    val endText = MutableLiveData<String>()
+    val durationText = MutableLiveData<String>()
+    val confirmClickAble = MutableLiveData<Boolean>()
+    val confirmText = MutableLiveData<String>()
+    override fun onCreated() {
+        confirmClickAble.value = true
+        confirmText.value = getString(R.string.ok)
+    }
+
+    fun onClickConfirm() {
+        mediaBean?.let {
+            viewModelScope.launch(Dispatchers.Main) {
+                val fileName = it.name
+                val gifName = if (fileName.contains(".")) {
+                    "${fileName.split(".")[0]}_${startPosition}_${endPosition}.gif"
+                } else {
+                    "${fileName}_${startPosition}_${endPosition}.gif"
+                }
+                val toFile = "${GifMakeHelper.gifDir}${File.separator}${gifName}"
+                if (FileUtils.checkExist(toFile)) {
+                    log("${toFile}已存在")
+                    return@launch
+                }
+                confirmClickAble.value = false
+                confirmText.value = "正在转换"
+                val result = GifMakeHelper.makeGif(mCxt, it.path, toFile, startPosition,  endPosition, 200) { current, total ->
+                    val logMsg = "$current/$total"
+                    log(logMsg)
+                    confirmText.postValue(logMsg)
+                }
+                log("完成：$result -> $toFile" )
+                if (result) {
+                    EventBus.getDefault().post(GifMakeEvent())
+                    delay(1000)
+                    finish()
+                } else  {
+                    com.android.absbase.utils.FileUtils.delete(File(toFile), true)
+                }
+                confirmClickAble.value = true
+            }
+        }
+
+    }
+}
